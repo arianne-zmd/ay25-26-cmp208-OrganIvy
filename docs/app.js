1326fc71f51496becb079c5fcd6ccb1a9b891382
Veronee-Maxius <vemaxius@salcc.edu.lc>
2026-04-08 15:16:27 -0400

    All pages current works but doesn't have any info yet.

--- docs/app.js
+++ docs/app.js
@@ -6,3 +6,25 @@ mMenu.addEventListener('click', function() {
     mMenu.classList.toggle('is-active');
     mMenuLinks.classList.toggle('active');
 } );
+
+/*animations*/
+gsap.registerPlugin(ScrollTrigger) 
+
+gsap.from('.main__content', {
+    duration: 1,
+    opacity:0,
+    y: -150,
+    stagger: 0.3
+});
+
+gsap.from('.services__animation', {
+     scrollTrigger: {
+        trigger: '.services__animation',
+        start: 'top 80%', // when element hits 80% of viewport
+    },
+    //ScrollTrigger: '.services__animation',
+    duration: 0.5,
+    opacity:0,
+    x: -150,
+    stagger: 0.1
+});
